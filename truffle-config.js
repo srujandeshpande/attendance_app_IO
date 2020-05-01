c5f2b912623d9f4fe514025fa8e3375886568224
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-01 23:18:36 +0530

    ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,13 @@ module.exports = {
       network_id: "*" // Match any network id
     }
   },
+  ropsten: {
+    provider: function() {
+      return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/YOUR_API_KEY")
+    },
+    network_id: 3,
+    gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+  },
   solc: {
     optimizer: {
       enabled: true,
